09c65dc9da5b8f63b9b74f55f0cf14ef8a6665e4
GuidoCurradi <Guidocurradi1998@gmail.com>
2023-06-07 10:54:44 +0200

    Modifica 3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 	
 	
 	public static void main (String[] args) {
-		System.out.println("Avvio programma main");		
+		System.out.println("Avvio programma main");	
+		System.out.println("Esecuzione del programma");	
+		System.out.println("Fine del programma");	
 	}
 
 }
